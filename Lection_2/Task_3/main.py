828bbb6c93f9728df5f74727b7fae513076041ca
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-20 20:51:08 +0100

    Lection_2 folder added

---
+++ Lection_2/Task_3/main.py
@@ -0,0 +1,14 @@
+first = int(input("First digit: "))
+second = int(input("Second digit: "))
+action = int(input("Choose action\n1) sum\n2)substract\n3)multiply\n4)divide\nYour choise: "))
+match action:
+    case 1:
+        print("Sum =",first+second)
+    case 2:
+        print("Distraction =", first-second)
+    case 3:
+        print("Multiplication =",first*second)
+    case 4:
+        print("Division =",first/second)
+
+    
