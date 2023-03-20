b17fca970a5518b8e3d9d31313500d923cd4bb06
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-20 20:25:55 +0100

    file updated

--- Lection_1/Task_2/main.py
+++ Lection_1/Task_2/main.py
@@ -1,16 +1,4 @@
-arr1 = input('l1:').split()
-arr2 = input('l2:').split()
-
-arr1.reverse()
-l1 = ''.join(arr1)
-l1 = l1.zfill(len(arr1))
-
-arr2.reverse()
-l2 = ''.join(arr2)
-l2 = l2.zfill(len(arr2))
-
-out = int(l1) + int(l2)
-print(list(str(out)))
-
-
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+print ("Good luck",name,surname)
 
