3f13c07bd1fc2e62281f80e7f7109cff55891c73
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-21 09:06:33 +0100

    File main.py in Lection_1/Task_3 folder was modified

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,12 +1,13 @@
 first = int(input("First digit: "))
 second = int(input("Second digit: "))
-action = int(input("Choose action\n1) sum\n2)substract\n3)multiply\n4)divide\nYour choise: "))
-match action:
-    case 1:
-        print("Sum =",first+second)
-    case 2:
-        print("Distraction =", first-second)
-    case 3:
-        print("Multiplication =",first*second)
-    case 4:
-        print("Division =",first/second)
+action = int(input("Choose action\n1)sum\n2)substract\n3)multiply\n4)divide\nYour choise: "))
+if action == 1:
+    print("Sum =",first+second)
+elif action == 2:
+    print("Distraction =", first-second)
+elif action == 3:
+    print("Multiplication =",first*second)
+elif action == 4:
+    print("Division =",first/second)
+else:
+    print("Wrong action!")
