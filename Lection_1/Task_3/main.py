b11a3051170d85b4096d6c1ae1929f6964e8bce0
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-23 13:36:38 +0100

    main.py in Lection_1/Task_3 was modified with using match statemant

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,13 +1,14 @@
 first = int(input("First digit: "))
 second = int(input("Second digit: "))
 action = int(input("Choose action\n1)sum\n2)substract\n3)multiply\n4)divide\nYour choice: "))
-if action == 1:
-    print("Sum =",first+second)
-elif action == 2:
-    print("Distraction =", first-second)
-elif action == 3:
-    print("Multiplication =",first*second)
-elif action == 4:
-    print("Division =",first/second)
-else:
-    print("Wrong action!")
+match action:
+    case 1:
+        print("Sum =",first+second)
+    case 2:
+        print("Distraction =", first-second)
+    case 3:
+        print("Multiplication =",first*second)
+    case 4:
+        print("Division =",first/second)
+    case _:
+        print("Wrong action!")
