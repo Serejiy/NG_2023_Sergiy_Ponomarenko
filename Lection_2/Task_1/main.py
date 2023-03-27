f5a89bd764ddb39a69f7f20c0cc10b0b0580103c
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-27 16:00:56 +0200

    Lection_2 folder added

---
+++ Lection_2/Task_1/main.py
@@ -0,0 +1,7 @@
+def count_elems():
+    N = int(input("Enter amount of elements"))
+    elements = []
+    for a in range(elements):
+        element = input("Enter element {a+1}")
+        elements.append(element)
+        a += 1
