00c784a314775c56611e37c77982fbc7cb346157
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-29 18:22:03 +0200

    Variable a changed to index

--- Lection_2/Task_2/main.py
+++ Lection_2/Task_2/main.py
@@ -1,6 +1,6 @@
 elements = input("Enter elements: ").split()
 unique_elements = []
-for a in elements:
-    if elements.count(a) == 1:
-        unique_elements.append(a)
+for index in elements:
+    if elements.count(index) == 1:
+        unique_elements.append(index)
 print("Unique elements: ",unique_elements)
