bf9da1b2676baee6f0269787bb306c70ace372a2
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-31 11:26:12 +0200

    main.py modified

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,8 +1,9 @@
 def count_elems():
-    amount = int(input("Enter amount of elements: "))
     elements = []
+    amount = int(input("Enter amount of elements: "))
     for number in range(amount):
-        element = input(f"Enter element {number+1}: ")
+        number +=1
+        element = input(f"{number}.Element: ")
         elements.append(element)
     count = input("Enter the name of element that should be counted: ")
     print("Amount of requested element: ", elements.count(count))
