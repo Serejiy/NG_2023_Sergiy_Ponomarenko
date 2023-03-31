708abfcec87342ab097a5f666e77cf7568369a9f
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-03-31 11:30:20 +0200

    main.py var unique_list changed to non_unique_list

--- Lection_2/Task_3/main.py
+++ Lection_2/Task_3/main.py
@@ -1,15 +1,15 @@
 set1 = input("Enter elements of the first set: ").split()
 set2 = input("Enter elements of the second set: ").split()
 set3 = input("Enter elements of the third set: ").split()
-unique_list = []
+non_unique_list = []
 for element in set1:
     if set1.count(element)>1:
-        unique_list.append(element)
+        non_unique_list.append(element)
 for element in set2:
     if set2.count (element)>1:
-        unique_list.append(element)
+        non_unique_list.append(element)
 for element in set3:
     if set3.count (element)>1:
-        unique_list.append(element)
+        non_unique_list.append(element)
 
-print ("The set of non-unique elements:",set(unique_list))
+print ("The set of non-unique elements:",set(non_unique_list))
