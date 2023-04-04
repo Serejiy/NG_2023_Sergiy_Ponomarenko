05c48760f9366070d7e9f0f9747b193459d8e5e7
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-04-04 19:09:23 +0200

    main.py added

---
+++ Lection_3/Task_4/main.py
@@ -0,0 +1,49 @@
+import requests
+
+def add_urls():
+    urls = []
+    elmnt_num = 0
+    lst_size = int(input("Enter the size of the list: "))
+    for url in range(lst_size):
+        elmnt_num += 1
+        input_url = input(f"Enter {elmnt_num} url: ")
+        urls.append(input_url)
+    return urls
+
+urls = add_urls()
+
+start = input("Urls added to the list.\nType 'y' to start checking: ")
+if start.lower() == 'y':
+    num = 0
+    status_codes = {
+        200: "OK",
+        201: "Created",
+        202: "Accepted",
+        204: "No Content",
+        301: "Moved Permanently",
+        302: "Found",
+        304: "Not Modified",
+        400: "Bad Request",
+        401: "Unauthorized",
+        403: "Forbidden",
+        404: "Not Found",
+        500: "Internal Server Error",
+        502: "Bad Gateway",
+        503: "Service Unavailable"
+    }
+    for url in urls:
+        num += 1
+        try:
+            response = requests.get(url)
+            if response.status_code in status_codes:
+                print(f"The status of the {num} url: {status_codes[response.status_code]}")
+            else:
+                print(f"The status of the {num} url: Unknown status code")
+        except requests.ConnectionError:
+            print(f"The {num} url is invalid or there is no internet connection")
+else:
+    print("Wrong input.")
+
+
+
+
