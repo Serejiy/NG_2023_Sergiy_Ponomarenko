33d402dc65656aaa3ad3c388333a440d50185cf1
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-04-29 12:30:24 +0200

    сделал проще

--- Lection_3/Task_2/main.py
+++ Lection_3/Task_2/main.py
@@ -1,12 +1,8 @@
 def print_rhombus(size, i=1):
     if i <= size:
-        num_spaces = abs(size - i)
-        num_stars = i
-        print(" " * num_spaces + "* " + "* " * (num_stars - 1))
+        print(" " * (size - i) + "* " * i)
         print_rhombus(size, i + 1)
-        print(" " * num_spaces + "* " + "* " * (num_stars - 1))
-    else:
-        return
-
+        print(" " * (size - i) + "* " * i)
+        
 size = int(input("Enter the size of the rhombus: "))
 print_rhombus(size)
