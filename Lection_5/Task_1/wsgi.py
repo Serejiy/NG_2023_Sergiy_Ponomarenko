2082424a848f1a0dd3c45ffd2cfe3ad205e14503
Sergiy Ponomarenko <nomi45148@gmail.com>
2023-05-24 21:23:32 +0200

    file renamed

--- Lection_5/Task_1/wsgi.py
+++ Lection_5/Task_1/wsgi.py
@@ -1,14 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, send, join_room, leave_room
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return '<h1>Hello world!</h1>'
+users = {}
+chat_rooms = set()
 
-@app.route('/about')
-def about():
-    return '<h1>About</h1>'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        users[username] = session.get('flask_session_id')
+        return render_template('chat.html', username=username)
+    return render_template('login.html')
+
+@socketio.on('message')
+def handle_message(message):
+    username = session['username']
+    send(username + ': ' + message, room=session['room'])
+
+@socketio.on('join')
+def join(data):
+    username = session['username']
+    room = data['room']
+    session['room'] = room
+    join_room(room)
+    send(username + ' joined the room ' + room, room=room)
+
+@socketio.on('leave')
+def leave(data):
+    username = session['username']
+    room = session['room']
+    leave_room(room)
+    send(username + ' left the room ' + room, room=room)
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
